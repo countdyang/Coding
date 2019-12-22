07918511d341ab67a0ee12d6e52d5e3567bf03e5
countdyang <countdyang@hotmail.com>
2019-12-22 11:15:37 +0800

    first time

---
+++ src/main/java/Result.java
@@ -0,0 +1,11 @@
+/**
+ * Created by countdyang on 12/21/2019.
+ */
+public class Result {
+    public static void main(String args[]){
+        Combination temp = new Combination();
+        int[] input = {1,3,4};
+        System.out.println(temp.letterCombination(input));
+
+    }
+}
---
+++ src/test/java/unitTest.java
@@ -0,0 +1,87 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by countdyang on 7/15/2018.
+ */
+public class unitTest {
+
+    Combination temp = new Combination();
+
+    @Test
+	public void testForTwoSingleDigit(){
+        int[] input = {2,3};
+        String[] exp = new String[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+	}
+
+    @Test
+    public void testForTwoSingleDigitContainZeroOne1(){
+        int[] input = {0,3};
+        String[] exp = new String[] { "d", "e", "f" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+
+    @Test
+    public void testForTwoSingleDigitContainZeroOne2(){
+        int[] input = {1,9};
+        String[] exp = new String[] { "w", "x", "y","z" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+
+    @Test
+    public void testForTwoSingleDigitContainZeroOne3(){
+        int[] input = {0,1};
+        String[] exp = new String[] { "" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+
+    @Test
+    public void testForSingleDigitMoreThanTwo(){
+        int[] input = {2,3,4};
+        String[] exp = new String[] { "adg", "adh","adi","aeg", "aeh","aei","afg", "afh","afi","bdg", "bdh","bdi","beg", "beh","bei","bfg","bfh","bfi", "cdg","cdh","cdi", "ceg", "ceh","cei","cfg","cfh","cfi" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+
+    @Test
+    public void testForSingleDigitSame(){
+        int[] input = {2,2};
+        String[] exp = new String[] { "aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+
+    @Test
+    public void testForMoreThanNineAndContainZeroOne(){
+        int[] input = {20,21};
+        String[] exp = new String[] { "aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+
+    @Test
+    public void testForMoreThanNine2(){
+        int[] input = {13,99};
+        String[] exp = new String[] { "dww", "dwx", "dwy", "dwz", "dxw", "dxx", "dxy", "dxz", "dyw", "dyx", "dyy", "dyz", "dzw", "dzx", "dzy", "dzz",
+                                      "eww", "ewx", "ewy", "ewz", "exw", "exx", "exy", "exz", "eyw", "eyx", "eyy", "eyz", "ezw", "ezx", "ezy", "ezz",
+                                      "fww", "fwx", "fwy", "fwz", "fxw", "fxx", "fxy", "fxz", "fyw", "fyx", "fyy", "fyz", "fzw", "fzx", "fzy", "fzz",};
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+
+    @Test
+    public void testForMoreThanNine3(){
+        int[] input = {0,1,67};
+        String[] exp = new String[] { "mp", "mq", "mr","ms", "np", "nq", "nr", "ns", "op", "oq", "or", "os" };
+        List<String> expResult = Arrays.asList(exp);
+        Assert.assertEquals(expResult,temp.letterCombination(input));
+    }
+}
